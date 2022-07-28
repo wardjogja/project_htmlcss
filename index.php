6f75b5e2f96f9eecdc20eb0c7d5fac2a27f779aa
wardi <wardmasterindo@gmail.com>
2022-07-28 07:35:17 +0700

    menambah file

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Advanced Golang Media</title>
+    <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div class="header-left">
+
+                </div>
+                <div class="header-right">
+
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="container2">
+        
+        <!-- halaman sebelah kiri -->
+            
+            <div class="main-left">
+                <div class="main-left-top">
+                    <ul>
+                        <li class=""><a href="#">Contact</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Why Advance Golang Media</a></li>
+                        <li><a href="#" >Home</a></li>
+                    </ul>
+                </div>
+                <div class="main-left-header">
+                    <div class="main-left-header-left">
+                        <h1>We give growth to your business</h1>
+                        <p>more advance in technology</p>
+                    </div>
+                    <div class="main-left-header-right">
+
+                    </div>
+                </div>
+                <div class="main-left-body">
+
+                </div>
+                <div class="main-left-footer">
+
+                </div>
+            </div>
+        <!-- halaman sebelah kanan -->
+            <div class="main-right">
+
+            </div>
+        </div> 
+        </main>
+    </body>
+</html>
